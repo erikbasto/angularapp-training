753525ad0e3e29ee1c65912314615beb0fd594ca
erikbasto <30513989+erikbasto@users.noreply.github.com>
2021-12-10 18:02:36 -0600

    End of lesson 1.15

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,29 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Api.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private readonly  IConfiguration _config;
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,7 +24,7 @@ namespace Api
 
             services.AddDbContext<DataContext>(options => 
             {
-                options.UseSqlite("Connection string");    
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));    
             });
 
             services.AddControllers();
