ce5173bd7fbf03a042d47a722773bdd16a2af405
erikbasto <erik.basto85@gmail.com>
2023-04-24 15:53:32 -0600

    secion19 finished

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -13,6 +13,7 @@ export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm?: NgForm
   @Input() username?: string;
   messageContent = '';
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -21,10 +22,11 @@ export class MemberMessagesComponent implements OnInit {
 
   sendMessage(){
     if (!this.username) return;
-
+    this.loading = true;
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm?.reset()
-    });
+    })
+    .finally(() => this.loading = false );
   }
 
 }
